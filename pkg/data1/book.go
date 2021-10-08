8b8aaa04581d911b39cc37d84f92ab3768746ab4
lf8r <subhajit.dasgupta@hpe.com>
2021-10-08 09:50:12 -0500

    Added book and some related types.

---
+++ pkg/data1/book.go
@@ -0,0 +1,30 @@
+// Copyright (C) Subhajit DasGupta 2021
+
+package data1
+
+import "time"
+
+// Resource is a base type which must be embedded in persisted structs.
+type Resource struct {
+	ID       string
+	Name     string
+	Created  time.Time
+	Modified time.Time
+}
+
+type Seller struct {
+	Name         string
+	PurchaseLink string
+}
+
+type Edition struct {
+	ISBN   string
+	Format string
+}
+
+type Book struct {
+	Title     string
+	Author    string
+	Publisher string
+	Editions  []Edition
+}
